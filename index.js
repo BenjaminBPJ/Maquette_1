619b22cc470337939763e028e743cab9fd78883e
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-02-03 10:06:19 +0100

    Reste responsive et quelques détails

---
+++ index.js
@@ -0,0 +1,11 @@
+$(function (){
+$('.slide').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow : '<i class="fas fa-chevron-left fa-3x fleche fleche-gauche-offre"></i>',
+  nextArrow  : '<i class="fas fa-chevron-right fa-3x fleche"></i>'
+});
+});
+
+
