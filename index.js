236b455479f528a994eb399eb1a2bf00e5747664
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-02-04 09:26:22 +0100

    réalisation du responsiv

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-$(function (){
-$('.slide').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  prevArrow : '<i class="fas fa-chevron-left fa-3x fleche fleche-gauche-offre"></i>',
-  nextArrow  : '<i class="fas fa-chevron-right fa-3x fleche"></i>'
-});
-});
+$(function () {
+  $('.slide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fas fa-chevron-left fa-3x fleche fleche-gauche-offre"></i>',
+    nextArrow: '<i class="fas fa-chevron-right fa-3x fleche"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 545,
+        settings: "unslick"
+      }
+    ]
+  });
 
-
+});
